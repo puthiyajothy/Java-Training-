44f86cc0c775f6b84bcd6d33fab397145ff0b062
puthiyajothy <saidputhi@gmail.com>
2020-02-04 23:33:17 +0530

    mavn project

---
+++ Maven-project/ProjectP1/TestFirst.java
@@ -0,0 +1,17 @@
+package Test.ProjectP1;
+
+public class TestFirst {
+
+	public static void main(String[] args) {
+		
+			sayHello();
+			
+		}
+
+		public static void sayHello() {
+			System.out.println("MyTest says hello!");
+		}
+
+	}
+
+
---
+++ Maven-project/ProjectP2/TestSecond.java
@@ -0,0 +1,19 @@
+package Test.ProjectP2;
+
+import Test.ProjectP1.TestFirst;
+
+public class TestSecond {
+
+	public static void main(String[] args) {
+		
+			sayHello();
+			TestFirst.sayHello();
+		}
+
+		public static void sayHello() {
+			System.out.println("MyTest2 says hello!");
+		}
+
+	}
+
+
